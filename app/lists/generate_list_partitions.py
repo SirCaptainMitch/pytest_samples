fa2ef767a88a9ff4a7b99cb793927e514854f79e
Mitchell Hamann <mitchell.hamann@GlobalTranz.com>
2021-09-21 12:20:47 -0500

    lunch break additions

--- app/lists/generate_list_partitions.py
+++ app/lists/generate_list_partitions.py
@@ -1,5 +1,17 @@
+"""
+break list into chunks based on paramter. 
+
+List comprehenstion probably best for this, handles odd / even numbered lists. 
+
+list[x:x + chunk_size] - select values from list based on range of X + the chunk size. 
+for x in range(0, len(list), chunk_size) - create the range, with an increment = to the chunk size
+  to determine the number of iterations required. 
+
+"""
+
 
 def generate_list_partitions(list, chunk_size):
-  return list
-
-print(generate_list_partitions([10, 20, 30, 40, 50, 60, 70, 80, 90, 100], 2))
+  
+  list_of_lists = [ list[x:x + chunk_size] for x in range(0, len(list), chunk_size) ]
+  
+  return list_of_lists 
--- tests/test_lists.py
+++ tests/test_lists.py
@@ -8,6 +8,7 @@ sys.path.append(parent)
 
 from app.lists.list_multplier import list_multplier
 from app.lists.find_second_largest_number import find_second_largest_number
+from app.lists.generate_list_partitions import generate_list_partitions
 
 class ListTest(unittest.TestCase):
   
@@ -20,4 +21,16 @@ class ListTest(unittest.TestCase):
     l = [1,2,3]
     expected_result = 2
     self.assertEqual(expected_result, find_second_largest_number(l))
+
+  def test_generate_list_partitions(self):
+    l = [1,2,3]
+    expected_result = [
+        [10, 20],
+        [30, 40],
+        [50, 60],
+        [70, 80],
+        [90, 100],
+        [110]
+      ]
+    self.assertEqual(expected_result, generate_list_partitions([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110], 2))
 
